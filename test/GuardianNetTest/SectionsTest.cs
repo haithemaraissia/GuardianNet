51b166a4a4aa26b603735ad2528ee130c2506d67
Szymon Uglis <szymon.uglis@hotmail.com>
2018-01-01 18:04:15 +0100

    Test for sections endpoint

---
+++ test/GuardianNetTest/SectionsTest.cs
@@ -0,0 +1,30 @@
+﻿// GuardianNet/GuardianNetTest/SectionsTest.cs
+// 
+// Created at: 01/01/2018
+// Author: Szymon 'l7ssha' Uglis
+
+using System.IO;
+using System.Threading.Tasks;
+using GuardianNet;
+using Xunit;
+
+namespace GuardianNetTest
+{
+    public class SectionsTest
+    {
+        public GuardianApi GetApi()
+        {
+            var token = File.ReadAllText("token.txt");
+            return new GuardianApi(token);
+        }
+
+        [Fact]
+        public async Task SearchingSectionTest()
+        {
+            var guard = GetApi();
+            var res = await guard.GetSectionsAsync("sport");
+
+            Assert.NotNull(res);
+        }
+    }
+}
