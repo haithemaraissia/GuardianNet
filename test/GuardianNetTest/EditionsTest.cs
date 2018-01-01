9ca7f07d1eb55ee5599770d9225d4987fc4ab3e1
Szymon Uglis <szymon.uglis@hotmail.com>
2018-01-01 17:32:48 +0100

    Test for Editions endpoint

---
+++ test/GuardianNetTest/EditionsTest.cs
@@ -0,0 +1,30 @@
+﻿// GuardianNet/GuardianNetTest/EditionsTest.cs
+// 
+// Created at: 01/01/2018
+// Author: Szymon 'l7ssha' Uglis
+
+using System.IO;
+using System.Threading.Tasks;
+using GuardianNet;
+using Xunit;
+
+namespace GuardianNetTest
+{
+    public class EditionsTest
+    {
+        public GuardianApi GetApi()
+        {
+            var token = File.ReadAllText("token.txt");
+            return new GuardianApi(token);
+        }
+
+        [Fact]
+        public async Task GetAllEditionsTest()
+        {
+            var guardian = GetApi();
+            var res = await guardian.GetEditionsAsync();
+
+            Assert.NotNull(res);
+        }
+    }
+}
