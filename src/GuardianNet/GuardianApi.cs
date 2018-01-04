72640a021b04a553d71c824064913ce9f46eea07
Szymon Uglis <szymon.uglis@hotmail.com>
2018-01-04 18:29:04 +0100

    Documentation to public methods

--- src/GuardianNet/GuardianApi.cs
+++ src/GuardianNet/GuardianApi.cs
@@ -17,30 +17,63 @@ namespace GuardianNet
     public sealed class GuardianApi
     {
         private readonly string _token;
-        private readonly SearchExecutor SearchExecutor = new SearchExecutor();
-        private readonly TagExecutor TagExecutor = new TagExecutor();
-        private readonly EditionsExecutor EditionsExecutor = new EditionsExecutor();
-        private readonly SectionsExecutor SectionExecutor = new SectionsExecutor();
+        private readonly SearchExecutor _searchExecutor = new SearchExecutor();
+        private readonly TagExecutor _tagExecutor = new TagExecutor();
+        private readonly EditionsExecutor _editionsExecutor = new EditionsExecutor();
+        private readonly SectionsExecutor _sectionExecutor = new SectionsExecutor();
         
+        /// <summary> </summary>
+        /// <param name="token">TheGuardian API Token</param>
         public GuardianApi(string token)
             => _token = token;
 
+        // Those comments are possibly written in wrong way. Pls fix it.
+
+        /// <summary>
+        /// Fetch Guardian database with specified query
+        /// </summary>
+        /// <param name="query">Phrase to search. Space in string will flag search query as exact</param>
+        /// <returns>Response object</returns>
         public async Task<SearchResponse> SearchAsync(string query)
-            => await SearchExecutor.Search(_token, query);
+            => await _searchExecutor.Search(_token, query);
 
+        /// <summary>
+        /// Search in API with SearchQuery object.
+        /// </summary>
+        /// <param name="query">SearchQuery object containing few parameters</param>
+        /// <returns>response object</returns>
         public async Task<SearchResponse> SearchAsync(SearchQuery query)
-            => await SearchExecutor.Search(_token, query);
+            => await _searchExecutor.Search(_token, query);
 
+        /// <summary>
+        /// Search in tags API endpoint.
+        /// </summary>
+        /// <param name="query">Phrase to search</param>
+        /// <returns>Response object</returns>
         public async Task<TagResponse> GetTagsAsync(string query)
-            => await TagExecutor.Search(_token, query);
+            => await _tagExecutor.Search(_token, query);
 
+        /// <summary>
+        /// Search in tags API endpoint with query object.
+        /// </summary>
+        /// <param name="query">Query object to search in enpoint</param>
+        /// <returns>Returns Response object</returns>
         public async Task<TagResponse> GetTagsAsync(TagQuery query)
-            => await TagExecutor.Search(_token, query);
+            => await _tagExecutor.Search(_token, query);
 
+        /// <summary>
+        /// Fetches editions from Guardian. For now we have 4 editions.
+        /// </summary>
+        /// <returns>IEnumerale of Edition object</returns>
         public async Task<IEnumerable<Edition>> GetEditionsAsync()
-            => await EditionsExecutor.GetEditions(_token);
+            => await _editionsExecutor.GetEditions(_token);
 
+        /// <summary>
+        /// Fetch sections within specified query
+        /// </summary>
+        /// <param name="query">Phrase to search</param>
+        /// <returns>IEnumerable of Section object</returns>
         public async Task<IEnumerable<Section>> GetSectionsAsync(string query)
-            => await SectionExecutor.Search(_token, query);
+            => await _sectionExecutor.Search(_token, query);
     }
 }
